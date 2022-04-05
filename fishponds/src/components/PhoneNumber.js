9e5d487a58c97d43d0e8ea62255e1859fda52c74
L36951 <80443759+L36951@users.noreply.github.com>
2022-04-05 22:42:27 +0800

    Delete fishponds directory

--- fishponds/src/components/PhoneNumber.js
+++
@@ -1,8 +0,0 @@
-import './PhoneNumber.css'
-import React from 'react';
-const PhoneNumber=({updatedAt})=>{
-    return( <div className="phoneNumber">
-    <p>Updated At: {updatedAt}</p>
-    </div>);
-};
-export default PhoneNumber;
--- fishponds/src/password.js
+++
@@ -1,7 +0,0 @@
-const PASSWORD_SECRET = 'FishFarming'
-
-// return password_secret
-export const getPasswordSecret = () => {
-    return PASSWORD_SECRET
-  };
-  
